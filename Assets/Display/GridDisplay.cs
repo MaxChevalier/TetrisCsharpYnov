22160de45e10dc3334d21633beaebbf13c377ed7
MaxChevalier <maxime.chevalier@ynov.com>
2022-09-24 13:30:30 +0200

    exepted }

--- Assets/Display/GridDisplay.cs
+++ Assets/Display/GridDisplay.cs
@@ -199,7 +199,6 @@ public class GridDisplay : MonoBehaviour
                     gameOver();
                     SetPieceColors();
                 }
-                }
                 else{
                     DownPiece();
                 }
