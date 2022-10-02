be5ac93cb5bd0bd1e5a3cf646cac5d5e6b254592
Lupit0t <paul.piauger@ynov.com>
2022-10-02 16:03:01 +0200

    Commentaire GameManagers

--- Assets/Display/GameManger.cs
+++ Assets/Display/GameManger.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public delegate void PiecePosed();
 
+public delegate void PiecePosed(); //delegate pour la pose de la pièce
+
+//classe le management du jeu
 class GameManager
 {
+
 
     public MoveSystem moveSystem;
     public Collider collider;
@@ -16,20 +19,24 @@ class GameManager
     private int lines = 0;
     private List<int> paque = new List<int>(){};
 
+    //constructeur
     public GameManager()
     {
         collider = new Collider();
         moveSystem = new MoveSystem(collider, this);
     }
 
+    //fonction pour generer une nouvelle pièce
     public Piece generatePiece()
     {
         if (paque.Count == 0)
         {
 
+            //on genere toute les pieces possible et on les stock dans une liste
             paque = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
             var count = paque.Count;
             var last = count - 1;
+            //on vient melanger la liste
             for (var i = 0; i < last; ++i)
             {
                 var r = UnityEngine.Random.Range(i, count);
@@ -39,16 +46,17 @@ class GameManager
             }
 
         }
+        //on genere les piece dans l'ordre de la liste melanger
         int nbtPiece = paque[0];
         paque.RemoveAt(0);
         return new Piece(nbtPiece);
     }
 
+    //fonction pour supprimer les ligne pleine et deplacer les ligne au dessus
     public GameStat BreakLine(List<List<SquareColor>> colors, double speed)
     {
         int nbligne = 0;
         // la fonction supprime les lignes pleines et les lignes au dessus descendent
-        // si plusieur ligne son supprmé on ajoute le nombre de ligne supprimé multiplié par 10 au score
         for (int i = 0; i < 22; i++)
         {
             bool isFull = true;
@@ -64,7 +72,7 @@ class GameManager
                 nbligne++;
                 lines += 1;
                 lvl = ((int)lines / 10) + 1;
-                speed = 100 - (Math.Pow(lvl, 1.2f) * 2.5f);
+                speed = 100 - (Math.Pow(lvl, 1.2f) * 2.5f); // vitesse de la piece
                 for (int k = i; k > 0; k--)
                 {
                     for (int l = 0; l < 10; l++)
@@ -83,7 +91,7 @@ class GameManager
         {
             
             
-            score += (int)(((nbligne * 100) + ((Math.Pow(nbligne, 2) / 2) * 10)) * lvl);
+            score += (int)(((nbligne * 100) + ((Math.Pow(nbligne, 2) / 2) * 10)) * lvl); //le score est calculer en fonction du nombre de ligne supprimer et du niveau
         }
         else if (nbligne == 1)
         {
@@ -92,6 +100,7 @@ class GameManager
         return new GameStat(score, lvl, speed);
     }
 
+    //fonction pour remettre les couleur a transparent
     public void RemovePieceColors(Piece piece, List<List<SquareColor>> colors)
     {
         if(colors[piece.cord1[0]][piece.cord1[1]] == piece.color){
@@ -109,6 +118,7 @@ class GameManager
         
     }
 
+    //fonction pour verifié si la partie est fini
     public bool isgameOver(Piece piece, List<List<SquareColor>> colors)
     {
         if (colors[piece.cord1[0]][piece.cord1[1]] != SquareColor.TRANSPARENT || colors[piece.cord2[0]][piece.cord2[1]] != SquareColor.TRANSPARENT || colors[piece.cord3[0]][piece.cord3[1]] != SquareColor.TRANSPARENT || colors[piece.cord4[0]][piece.cord4[1]] != SquareColor.TRANSPARENT)
@@ -118,6 +128,8 @@ class GameManager
         return false;
 
     }
+
+    //fonciton pour colorier la piece
     public void SetPieceColors(Piece piece, List<List<SquareColor>> colors)
     {
         if(colors[piece.cord1[0]][piece.cord1[1]] == SquareColor.TRANSPARENT || colors[piece.cord1[0]][piece.cord1[1]] == SquareColor.PREVIEW){
